ce49a19d37e1a04ac6787e9d696abb0e5df89cc7
bhargavi <bhargaviraja2006@gmail.com>
2026-06-22 12:04:26 +0530

    task2

---
+++ task2/task2.js
@@ -0,0 +1,35 @@
+function loadUsers(){
+
+    document.getElementById("status").innerHTML = "Loading...";
+    document.getElementById("users").innerHTML = "";
+
+    setTimeout(() => {
+
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(response => response.json())
+        .then(data => {
+
+            let output = "";
+
+            data.forEach(user => {
+                output += `
+                <div class="user">
+                    <h3>${user.name}</h3>
+                    <p><b>Email:</b> ${user.email}</p>
+                    <p><b>Phone:</b> ${user.phone}</p>
+                </div>
+                `;
+            });
+
+            document.getElementById("users").innerHTML = output;
+
+            document.getElementById("status").innerHTML = "Loaded successfully";
+
+        })
+        .catch(error => {
+            document.getElementById("status").innerHTML = "Error loading data";
+            console.log(error);
+        });
+
+    }, 2000);
+}
